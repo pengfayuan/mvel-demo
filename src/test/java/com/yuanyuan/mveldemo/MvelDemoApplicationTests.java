c3c8deafc1f115fe2cc53e3dbdf2df9ea780a0c7
fayuan.peng <fa@mycredigo.com>
2021-09-03 23:52:48 +0800

    init project

---
+++ src/test/java/com/yuanyuan/mveldemo/MvelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanyuan.mveldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
